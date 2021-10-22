b4c89923cf08e1efbf93b3e3b204ceb2db3844d7
jcaswell <jakecaswell88@gmail.com>
2021-10-22 00:02:57 -0400

    added prototype methods to player.js

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -5,9 +5,9 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
 test('creates a player object', () => {
-    const player = new Player('Jake');
+    const player = new Player('Dave');
 
-    expect(player.name).toBe('Jake');
+    expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
@@ -16,4 +16,23 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 
+})
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+})
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 })
