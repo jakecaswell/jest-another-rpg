fc9b5ef8fcf67a88c853292a7d67f0975a7d3a96
jcaswell <jakecaswell88@gmail.com>
2021-10-23 22:35:29 -0400

    added functions for attacking and using and adding potions to the player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -65,3 +65,30 @@ test("subtracts from player's health", () => {
 
     expect(player.health).toBe(0);
 })
+
+test("gets player's attack value", () => {
+    const player = new Player('Zach');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+})
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Drew');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+})
+
+test('uses a potion from the inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+})
